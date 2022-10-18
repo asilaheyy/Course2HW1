4fa8495b39be8f7443e06088b700239de04ee657
Alisa Alexandrova <alisaalexandrova2001@gmail.com>
2022-10-18 16:13:08 +0300

    methods

--- src/Human.java
+++ src/Human.java
@@ -5,28 +5,17 @@ public class Human {
     String job;
 
     Human(String name, String hometown, int age, String job) {
-        if (name == null || name.isEmpty()) {
-            this.name = "*Информация не указана*";
-        } else {
-            this.name = name;
-        }
-        if (hometown == null || hometown.isEmpty()) {
-            this.hometown = "*Информация не указана*";
-        } else {
-            this.hometown = hometown;
-        }
-        if (age >= 0) {
-            this.age = age;
-        } else {
-            this.age = 0;
-        }
-        if (job == null || job.isEmpty()) {
-            this.job = "*Информация не указана*";
-        } else {
-            this.job = job;
-        }
+        this.name = name;
+        nameNull();
+        this.hometown = hometown;
+        hometownNull();
+        this.age = age;
+        ageNull();
+        this.job = job;
+        jobNull();
     }
-    Human(){
+
+    Human() {
         this.name = "*Информация не указана*";
         this.hometown = "*Информация не указана*";
         this.job = "*Информация не указана*";
@@ -36,5 +25,37 @@ public class Human {
     public void introduce() {
         int yearOfBirth = 2022 - age;
         System.out.println("Привет! Меня зовут " + name + ". Я из города " + hometown + ". Я родился/родилась в " + yearOfBirth + " году. Я работаю на должности " + job + ". Будем знакомы!");
+    }
+
+    public void nameNull() {
+        if (name == null || name.isEmpty()) {
+            this.name = "*Информация не указана*";
+        } else {
+            this.name = name;
+        }
+    }
+
+    public void hometownNull() {
+        if (hometown == null || hometown.isEmpty()) {
+            this.hometown = "*Информация не указана*";
+        } else {
+            this.hometown = hometown;
+        }
+    }
+
+    public void ageNull() {
+        if (age >= 0) {
+            this.age = age;
+        } else {
+            this.age = 0;
+        }
+    }
+
+    public void jobNull() {
+        if (job == null || job.isEmpty()) {
+            this.job = "*Информация не указана*";
+        } else {
+            this.job = job;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Human Maxim = new Human();
         Human Anna = new Human("Аня","Москва", 29,"методист образовательных программ");
-        Human Ekaterina = new Human("Катя","", 28, "продакт-менеджер");
-        Human Artem = new Human("Артем", "Москва", 27,"директор по развитию бизнеса");
+        Human Ekaterina = new Human("Катя", "", 26, "продакт-менеджер");
+        Human Artem = new Human("", null, 27,"директор по развитию бизнеса");
 
         Artem.introduce();
         Anna.introduce();
